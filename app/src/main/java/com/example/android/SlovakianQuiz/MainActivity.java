ef5210d4cd371bd6e1678e0beece88a25a4cd937
JergusSemko <jr.semko@gmail.com>
2018-02-21 05:16:37 +0100

    Addressed all the problems.

--- app/src/main/java/com/example/android/SlovakianQuiz/MainActivity.java
+++ app/src/main/java/com/example/android/SlovakianQuiz/MainActivity.java
@@ -1,32 +1,82 @@
 package com.example.android.SlovakianQuiz;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int points;
+    RadioButton q1Answer1;
+    RadioButton q2Answer2;
+    RadioButton q3Answer1;
+    RadioButton q4Answer1;
+    RadioButton q5Answer3;
+    EditText answerQ7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*The answer for the last question is ready from the beginning of the app
+            A little hint, the answer is "5 million" but I am sure you can find it
+            in the code anyway ;)
+         */
+        answerQ7 = findViewById(R.id.answer_edit_text_7);
     }
 
-    public void letsStart(View v) {
-        Intent start = new Intent(MainActivity.this, questions.class);
-        startActivity(start);
+    public void submit(View view) {
 
-        EditText name = findViewById(R.id.enteredName);
-        String nameString = name.getText().toString();
-        Intent intent = new Intent(getApplicationContext(), questions.class);
-        intent.putExtra("name", nameString);
-        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
-        SharedPreferences.Editor editor = pref.edit();
-        editor.putString("name", nameString);
-        editor.apply();
-        startActivity(intent);
+        //This method takes care of pretty much everything
+        points = 0;
+        q1Answer1 = findViewById(R.id.q1_answer1);
+        q2Answer2 = findViewById(R.id.q2_answer2);
+        q3Answer1 = findViewById(R.id.q3_answer1);
+        q4Answer1 = findViewById(R.id.q4_answer1);
+        q5Answer3 = findViewById(R.id.q5_answer3);
+
+        //Here are the lines for question 6 concerning check box part of the app
+        CheckBox q6Answer1 = findViewById(R.id.q6_answer1);
+        boolean hasQ6Option1 = q6Answer1.isChecked();
+        Log.v("MainActivity", "Correct q6Option1: " + hasQ6Option1);
+
+        CheckBox q6Answer2 = findViewById(R.id.q6_answer2);
+        boolean hasQ6Option2 = q6Answer2.isChecked();
+        Log.v("MainActivity", "Correct q6Option2: " + hasQ6Option2);
+
+        CheckBox q6Answer3 = findViewById(R.id.q6_answer3);
+        boolean hasQ6Option3 = q6Answer3.isChecked();
+        Log.v("MainActivity", "Correct q6Option3: " + hasQ6Option3);
+
+        //Final counting of all of the points happens on submit button click
+        if (q1Answer1.isChecked()) {
+            points += 1;
+        }
+        if (q2Answer2.isChecked()) {
+            points += 1;
+        }
+        if (q3Answer1.isChecked()) {
+            points += 1;
+        }
+        if (q4Answer1.isChecked()) {
+            points += 1;
+        }
+        if (q5Answer3.isChecked()) {
+            points += 1;
+        }
+        if (hasQ6Option2 && hasQ6Option3 && !hasQ6Option1) {
+            points += 1;
+        }
+        if (answerQ7.getText().toString().trim().equalsIgnoreCase(getString(R.string.texta))) {
+            points += 1;
+        }
+
+        Toast.makeText(getApplicationContext(), "Your score is " + points, Toast.LENGTH_SHORT).show();
     }
 }
